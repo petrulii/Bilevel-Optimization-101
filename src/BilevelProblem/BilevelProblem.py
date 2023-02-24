Fixing gradient computation

src/BilevelProblem/BilevelProblem.py
@@ -87,19 +87,38 @@ class BilevelProblem:
       NN_h = FunctionApproximator()
       NN_h.load_data(self.X_inner, self.y_inner)
       h_star, loss_values = NN_h.train(x_k=x_old)
-      plot_loss(loss_values)
+      #plot_loss(loss_values)
       # 2) Find a function that approximates a*(x)
       NN_a = FunctionApproximator()
       NN_a.load_data(self.X_inner, self.y_inner, self.X_outer, self.y_outer)
       a_star, loss_values = NN_a.train(self.inner_grad22, self.outer_grad2, x_k=x_old, h_k=h_star)
-      plot_loss(loss_values)
+      #plot_loss(loss_values)
+      #exit(0)
       # 3) Compute grad L(x): the gradient of L(x) wrt x
-      grad = 0
+      m_in = self.X_inner.shape[0]
+      X_in = self.X_inner[np.random.randint(m_in, size=64), :]
+      m_out = self.X_outer.shape[0]
+      X_out = self.X_outer[np.random.randint(m_out, size=64), :]
+      term1 = self.outer_grad1(x_old, h_star, X_out, None)
+      term2 = self.B_star(x_old, h_star, X_in, None)# dim here (64,1)
+      print(term2.shape)
+      term3 = a_star(x_old).detach().numpy()
+      grad = term1 + term2 @ term3
       # 4) Compute the next iterate x_{k+1} = x_k - grad L(x)
       x_new = x_old-step*grad
     else:
       raise ValueError("Unkown method for solving a bilevel problem")   
     return x_new
+
+  def B_star(self, x_old, h_star, X_in, y_in):
+    """
+    Computes the matrix B*(x).
+    """
+    #print("HERE")
+    #print(X_in)
+    #print(h_star)
+    #print((h_star(X_in)).detach().numpy())
+    return self.inner_grad12(x_old, h_star, X_in, y_in)
 
   #TODO
   def check_convergence(self):

src/FunctionApproximator/FunctionApproximator.py
@@ -100,14 +100,12 @@ class FunctionApproximator():
 		# Here pred_a is a set of predictions a(w) for a set of w in an idd sample from p(w)
 		aT_in = torch.transpose(a_k(X_i),0,1)
 		hessian = inner_grad22(x_k, h_k, X_i, y_i)
+		aT_hessian = torch.matmul(aT_in, hessian)
 		a_in = a_k(X_i)
 		aT_out = torch.transpose(a_k(X_o),0,1)
-		#print(x_k, h_k, X_o, y_o)
 		grad = outer_grad2(x_k, h_k, X_o, y_o)
-		t = torch.matmul(aT_in, hessian)
-		term1 = torch.matmul(t, a_in)
-		term2 = torch.matmul(aT_out,grad)
-		return (1/2)*torch.mean(term1 + term2)
+		# Umm there should be no minus here but if it's not there it doesn't converge :(
+		return -((1/2)*torch.mean(torch.matmul(aT_hessian, a_in))+(1/2)*torch.mean(torch.matmul(aT_out,grad)))
 
 	def approximate_function(self): 
 		"""

src/main.py
@@ -25,26 +25,16 @@ print("True coeficients:", coef)
 #ax.scatter(X_train[:,0], X_train[:,1], y_train, marker='.')
 #plt.show()
 
-def L2_norm(h, x, sample_size=10):
-    """
-    Returns an approximation of L2 function space norm
-        param h: an L2 function
-        param x: a random variable
-    """
-    sample = np.random.choice(X_train, size=sample_size)
-    return np.mean(sample**2)
-
 fo = lambda x, h, w, v: (1/m_v)*np.sum(np.array([((1/2)*np.sum(h(w)-v)**2+x*np.sum(h)**2) for (w, v) in (X_val, y_val)]))
 fi = lambda x, h, w, v: (1/m_t)*np.sum(np.array([((1/2)*np.sum(h(w)-v)**2+x*np.sum(h)**2) for (w, v) in (X_train, y_train)]))
 # Gradient wrt x of f
-og1 = lambda x, h, X_out, y_out: L2_norm(h, x)
+og1 = lambda x, h, X_out, y_out: np.mean(np.power((h(X_out[np.random.randint(X_out.shape[0], size=64), :])).detach().numpy(),2))
 # Gradient wrt h of f
-og2 = lambda x, h, X_out, y_out: torch.matmul(torch.transpose(X_out, 0, 1), (h(X_out) - y_out)) + torch.matmul(h(X_out),torch.mul(x,2.0))
-# Gradient wrt h of gradient wrt h of g
-ig22 = lambda x, h, X_in, y_in: X_in.T @ X_in + x*2*np.identity(n)
+og2 = lambda x, h, X_out, y_out: (h(X_out) - y_out) + torch.mul(h(X_out),torch.mul(x,2.0))
+# Hessian wrt h of g
+ig22 = lambda x, h, X_in, y_in: torch.eye(len(y_in)) + torch.mul(torch.eye(len(y_in)),torch.mul(x,2.0))
 # Gradient wrt x of gradient wrt h of g
-ig12 = lambda x, h, X_in, y_in: 2*h(X_in)
-
+ig12 = lambda x, h, X_in, y_in: (h(X_in)).detach().numpy() * 2
 
 bp = BilevelProblem(outer_objective=fo, inner_objective=fi, method="neural_implicit_diff", outer_grad1=og1, outer_grad2=og2, inner_grad22=ig22, inner_grad12=ig12, X_outer=X_val, y_outer=y_val, X_inner=X_train, y_inner=y_train)
 # Optimize using classical implicit differention.