Big fix, gradients were wrong

src/BilevelProblem/BilevelProblem.py
@@ -1,9 +1,10 @@
 import math
+import time
 import torch
 import numpy as np
-from utils import plot_loss
-from utils import sample
 import matplotlib.pyplot as plt
+from utils import plot_2D_functions
+from utils import sample_X, sample_X_y
 from FunctionApproximator.FunctionApproximator import FunctionApproximator
 
 
@@ -39,10 +40,10 @@ class BilevelProblem:
     self.y_inner = y_inner
     self.batch_size = batch_size
     self.find_h_star = find_h_star
-    if self.method=="neural_implicit_diff":
-      self.NN_h = FunctionApproximator()
+    if self.method=="neural_implicit_diff" or self.method=="frankenstein":
+      self.NN_h = FunctionApproximator(function='a')
       self.NN_h.load_data(self.X_inner, self.y_inner)
-      self.NN_a = FunctionApproximator()
+      self.NN_a = FunctionApproximator(function='a')
       self.NN_a.load_data(self.X_inner, self.y_inner, self.X_outer, self.y_outer)
     self.__input_check__()
 
@@ -56,7 +57,7 @@ class BilevelProblem:
       raise AttributeError("You must specify each of the necessary gradients")
     if (self.method == "implicit_diff")  and (self.find_h_star is None):
       raise AttributeError("You must specify the closed form solution of the inner problem for classical imp. diff.")
-    if not (self.method == "implicit_diff" or self.method == "neural_implicit_diff")  or (self.method is None):
+    if not (self.method == "implicit_diff" or self.method == "neural_implicit_diff" or self.method == "frankenstein")  or (self.method is None):
       raise ValueError("Invalid method for solving the bilevel problem")
 
   def optimize(self, mu0, maxiter=100, step=0.1):
@@ -68,17 +69,17 @@ class BilevelProblem:
     """
     if not isinstance(mu0, (torch.Tensor)):
       raise TypeError("Invalid input type for mu0, should be a tensor")
-    n_iters = 0
-    converged = False
     mu_new = mu0
-    iters = [mu_new]
-    while n_iters < maxiter and converged is False:
+    n_iters, iters, converged, times = 0, [mu_new], False, []
+    while n_iters < maxiter and not converged:
       mu_old = mu_new.clone()
-      mu_new = self.find_mu_new(mu_old, step)
+      start = time.time()
+      mu_new, h_star = self.find_mu_new(mu_old, step)
+      times.append(time.time() - start)
       converged = self.check_convergence(mu_old, mu_new)
       iters.append(mu_new)
       n_iters += 1
-    return mu_new, iters, n_iters
+    return mu_new, iters, n_iters, times, h_star
 
   def find_mu_new(self, mu_old, step):
     """
@@ -92,21 +93,39 @@ class BilevelProblem:
       # 1) Find a parameter vector h* the argmin of the inner objective G(mu,h)
       h_star = self.find_h_star(self.X_inner, self.y_inner, mu_old)
       # 2) Get Jh* the Jacobian of the inner objective wrt h
-      Jac = (-1*torch.linalg.inv(self.inner_grad22(mu_old, h_star, X_in, y_in))) @ (self.inner_grad12(mu_old, h_star, X_in, y_in))
+      Jac = -1*torch.linalg.solve((self.inner_grad22(mu_old, h_star, X_in, y_in)), (self.inner_grad12(mu_old, h_star, X_in, y_in)))
       # 3) Compute grad L(mu): the gradient of L(mu) wrt mu
-      term1 = self.outer_grad1(mu_old, h_star, X_out, y_out)
-      term2 = torch.transpose(Jac,0,1) @ self.outer_grad2(mu_old, h_star, X_out, y_out)
-      grad = (term1 + term2)
+      grad = self.outer_grad1(mu_old, h_star, X_out, y_out) + Jac.T @ self.outer_grad2(mu_old, h_star, X_out, y_out)
+    elif self.method=="frankenstein":
+      X_in, y_in = self.X_inner, self.y_inner
+      X_out, y_out = self.X_outer, self.y_outer
+      # 1) Find a parameter vector h* the argmin of the inner objective G(mu,h)
+      h_star_vec = self.find_h_star(self.X_inner, self.y_inner, mu_old)
+      h_star = lambda x : x @ h_star_vec
+      # 2) Find a function that approximates a*(x)
+      a_star, loss_values = self.NN_a.train(self.inner_grad22, self.outer_grad2, mu_k=mu_old, h_k=h_star, num_epochs = 10)
+      X_out, y_out = sample_X_y(self.X_outer, self.y_outer, self.batch_size)
+      # 3) Compute grad L(x): the gradient of L(x) wrt x
+      X_in = sample_X(self.X_inner, self.batch_size)
+      X_out = sample_X(self.X_outer, self.batch_size)
+      term1 = self.outer_grad1(mu_old, h_star, X_out, None)
+      term2 = self.B_star(mu_old, h_star, X_in, None)
+      term3 = a_star(X_in)
+      grad = term1 + term2.T @ term3
     elif self.method=="neural_implicit_diff":
       # 1) Find a function that approximates h*(x)
       h_star, loss_values = self.NN_h.train(mu_k=mu_old, num_epochs = 10)
       #plot_loss(loss_values)
       # 2) Find a function that approximates a*(x)
       a_star, loss_values = self.NN_a.train(self.inner_grad22, self.outer_grad2, mu_k=mu_old, h_k=h_star, num_epochs = 10)
+      X_out, y_out = sample_X_y(self.X_outer, self.y_outer, self.batch_size)
+      #true_a_star = self.find_a_star(h_star, X_out, y_out)
+      #print("a_star:", a_star(X_out))
+      #print("true_a_star:", true_a_star)
       #plot_loss(loss_values)
       # 3) Compute grad L(x): the gradient of L(x) wrt x
-      X_in = sample(self.X_inner, self.batch_size)
-      X_out = sample(self.X_outer, self.batch_size)
+      X_in = sample_X(self.X_inner, self.batch_size)
+      X_out = sample_X(self.X_outer, self.batch_size)
       term1 = self.outer_grad1(mu_old, h_star, X_out, None)
       term2 = self.B_star(mu_old, h_star, X_in, None)
       term3 = a_star(X_in)
@@ -114,18 +133,25 @@ class BilevelProblem:
     else:
       raise ValueError("Unkown method for solving a bilevel problem")
     # 4) Compute the next iterate x_{k+1} = x_k - grad L(x)
+    #print("Gradient:", grad)
     mu_new = mu_old-step*grad
     # 5) Enforce x positive
     mu_new = torch.full((1,1), 0.) if mu_new[0,0]<0 else mu_new
     # Remove the associated autograd
     mu_new = mu_new.detach()
-    return mu_new
+    return mu_new, h_star
 
   def B_star(self, mu_old, h_star, X_in, y_in):
     """
     Computes the matrix B*(x).
     """
     return self.inner_grad12(mu_old, h_star, X_in, y_in)
+
+  def find_a_star(self, h, X_out, y_out):
+    """
+    Computes the matrix B*(x).
+    """
+    return -torch.mean(h(X_out) - y_out)
 
   def check_convergence(self, mu_old, mu_new):
     """

src/FunctionApproximator/FunctionApproximator.py
@@ -8,14 +8,19 @@ class FunctionApproximator():
 	An object to approximate an arbitrary function.
 	"""
 
-	def __init__(self, layer_sizes=[2, 10, 20, 10, 1], batch_size = 64):
+	def __init__(self, layer_sizes=[2, 10, 20, 10, 1], batch_size = 64, function='h'):
 		"""
 		Init method.
 			param layer_sizes: sizes of the layers of the network used to approximate functions
 			param batch_size: size of training batches
 		"""
 		self.batch_size = batch_size
-		self.NN = NeuralNetwork(layer_sizes)
+		if function == 'h':
+			self.NN = NeuralNetwork_h()
+		elif function == 'a':
+			self.NN = NeuralNetwork_a(layer_sizes)
+		else:
+			raise AttributeError("You must specify the function that the network will approximate.")
 
 	def load_data(self, X_inner, y_inner, X_outer=None, y_outer=None):
 		"""
@@ -62,12 +67,12 @@ class FunctionApproximator():
 		elif not (mu_k is None and h_k is None and self.X_inner is None and self.y_inner is None and self.X_outer is None and self.y_outer is None):
 			# Set the loss H with a fixed h*(x) as the objective function
 			for epoch in range(num_epochs):
-				for i, ((X_i, y_i), (X_o, y_o)) in enumerate(zip(self.inner_dataloader, self.outer_dataloader)): # FIX THE DATALOADER
+				for i, ((X_i, y_i), (X_o, y_o)) in enumerate(zip(self.inner_dataloader, self.outer_dataloader)):
 					# Zero all the parameter gradients
-					optimizer.zero_grad()
 					a_k = self.NN
 					loss = self.loss_H(mu_k, h_k, a_k, inner_grad22, outer_grad2, X_i, y_i, X_o, y_o)
 					loss_values.append(loss.float())
+					optimizer.zero_grad()
 					loss.backward()
 					optimizer.step()
 			return self.NN, loss_values
@@ -95,14 +100,14 @@ class FunctionApproximator():
 		return (1/2)*torch.mean(torch.matmul(aT_hessian.double(), a_in.double()))+(1/2)*torch.mean(torch.matmul(aT_out.double(),grad.double()))
 
 
-class NeuralNetwork(nn.Module):
+class NeuralNetwork_a(nn.Module):
 	"""
-	A neural network to approximate an arbitrary function.
+	A neural network to approximate the function a* for neural implicit differentiation.
 	"""
 	def __init__(self, layer_sizes):
 		if len(layer_sizes) != 5:
 			raise ValueError("The network has five layers, you must give a list with five integer values")
-		super(NeuralNetwork, self).__init__()
+		super(NeuralNetwork_a, self).__init__()
 		self.layer_1 = nn.Linear(layer_sizes[0], layer_sizes[1])
 		nn.init.kaiming_uniform_(self.layer_1.weight)
 		self.layer_2 = nn.Linear(layer_sizes[1], layer_sizes[2])
@@ -118,6 +123,20 @@ class NeuralNetwork(nn.Module):
 		x = self.layer_4(x)
 		return x
 
+class NeuralNetwork_h(nn.Module):
+	"""
+	A neural network to approximate the function h* for neural implicit differentiation.
+	"""
+	def __init__(self):
+		super(NeuralNetwork_h, self).__init__()
+		self.layer_1 = nn.Linear(2, 2)
+		nn.init.kaiming_uniform_(self.layer_1.weight)
+		self.layer_2 = nn.Linear(2, 1)
+
+	def forward(self, x):
+		x = self.layer_1(x)
+		x = self.layer_2(x)
+		return x
 
 class Data(Dataset):
 	"""