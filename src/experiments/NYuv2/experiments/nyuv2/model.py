Segmentation with auxiliary tasks backprop bug fixed

src/experiments/NYuv2/experiments/nyuv2/model.py
@@ -110,9 +110,8 @@ class SegNetSplit(nn.Module):
         t3_pred = self.pred_task3(g_decoder[i][1])
         t3_pred = t3_pred / torch.norm(t3_pred, p=2, dim=1, keepdim=True)
 
-        out = [t1_pred, t2_pred, t3_pred]
 
         if self.logsigma is not None:
-            return out, self.logsigma
+            return t1_pred, t2_pred, t3_pred, self.logsigma
         else:
-            return out
+            return t1_pred, t2_pred, t3_pred

src/model/BilevelProblem/BilevelProblem.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import torch
+from torch.nn import functional as func
 
 # Add main project directory path
 sys.path.append('/home/clear/ipetruli/projects/bilevel-optimization/src')
@@ -9,6 +10,7 @@ from model.InnerSolution.InnerSolution import InnerSolution
 
 from sklearn.metrics import accuracy_score
 
+from torchviz import make_dot
 
 class BilevelProblem:
   """
@@ -32,7 +34,7 @@ class BilevelProblem:
     self.inner_dataloader = inner_dataloader
     self.batch_size = batch_size
     self.device = device
-    self.outer_model, self.outer_optimizer = outer_model
+    self.outer_model, self.outer_optimizer, self.outer_scheduler = outer_model
     self.inner_solution = InnerSolution(inner_loss, inner_dataloader, inner_models, device)
 
   def __input_check__(self, outer_loss, inner_loss, outer_dataloader, inner_dataloader, outer_model, inner_models, device, batch_size):
@@ -43,33 +45,34 @@ class BilevelProblem:
       raise AttributeError("You must specify inner and outer losses.")
     if (outer_dataloader is None) or (inner_dataloader is None):
       raise AttributeError("You must give outer and inner dataloaders.")
-    if (outer_model is None) or not(len(outer_model)==2):
+    if (outer_model is None) or not(len(outer_model)==3):
       raise AttributeError("You must specify the outer model and its optimizer.")
-    if (inner_models is None) or not(len(inner_models)==4):
+    if (inner_models is None) or not(len(inner_models)==6):
       raise AttributeError("You must specify both inner models and their optimizers.")
     if (device is None):
       raise AttributeError("You must specify a device: GPU/CPU.")
     if not (type(batch_size) is int):
       raise TypeError("Batch size must be an integer value.")
 
-  def optimize(self, mu, max_epochs=1, outer_optimizer=None):
+  def optimize(self, mu, max_epochs=1, outer_optimizer=None, test_dataloader=None):
     """
     Find the optimal outer solution.
       param mu: initial value of the outer variable
       param maxiter: maximum number of iterations
     """
-    running_loss, nb_iters, iters, losses, times = 0, 0, [], [], []
+    iters, outer_losses, inner_losses, test_losses, times = 0, [], [], [], []
     # Making sure gradient of mu is computed.
     for epoch in range(max_epochs):
+      epoch_iters = 0
+      epoch_loss = 0
       #for X_outer, y_outer in self.outer_dataloader:
       # _____NYUv2 start_____
       for item in self.outer_dataloader:
+        start = time.time()
         eval_data, eval_label, eval_depth, eval_normal = item
         X_outer = eval_data
         y_outer = (eval_label, eval_depth, eval_normal)
       # _____NYUv2 end_____
-        start = time.time()
-        iters.append(torch.flatten(mu.detach().clone()))
         # Move to GPU
         X_outer = X_outer.to(self.device)
         if type(y_outer) is tuple:
@@ -83,52 +86,74 @@ class BilevelProblem:
         # Inner value corresponds to h*(X_outer)
         mu.requires_grad = True
         inner_value = self.inner_solution(mu, X_outer, y_outer)
-        for item in inner_value:
-          item.requires_grad=True
         loss = self.outer_loss(mu, inner_value, y_outer)
+        #make_dot(loss, params=dict([('mu', mu)])).render("graph_loss", format="png")
+        print("Outer loss:", loss)
         # Backpropagation
         self.outer_optimizer.zero_grad()
         loss.backward()
+        print("Gradient of mu:", mu.grad)
         self.outer_optimizer.step()
-        times.append(time.time() - start)
-        running_loss += loss.item()
-        nb_iters += 1
-        print("Outer train iteration:", nb_iters, "\nRunning loss:", running_loss/nb_iters)
-        losses.append(running_loss/nb_iters)
-    return nb_iters, iters, losses, times
+        self.outer_scheduler.step()
+        # Update loss and iteration count
+        epoch_loss += loss.item()
+        epoch_iters += 1
+        iters += 1
+        duration = time.time() - start
+        times.append(duration)
+      # Evaluate at the end of every epoch
+      outer_losses.append(epoch_loss/epoch_iters)
+      print("Outer iteration:", epoch, "|", iters)
+      print("Outer iteration avg. loss:", epoch_loss/epoch_iters)
+      print("Inner iteration avg. loss:", self.inner_solution.loss)
+      inner_losses.append(self.inner_solution.loss)
+      print("Inner dual iteration avg. loss:", self.inner_solution.dual_loss)
+      test_loss, accuracy = self.evaluate(test_dataloader, mu)
+      test_losses.append(test_loss)
+      print("Test avg. loss:", test_loss)
+      print("Test avg. acc.:", accuracy)
+    return iters, outer_losses, inner_losses, test_losses, times
   
-  def evaluate(self, test_dataloader, mu):
+  def evaluate(self, test_dataloader, mu, max_iters=10):
     """
     Find the optimal outer solution.
-      param mu: outer variable
       param test_dataloader: test data
+      param mu: outer variable
     """
-    running_loss, nb_iters, losses = 0, 0, []
-    #for X_outer, y_outer in self.outer_dataloader:
+    total_loss, total_acc, iters = 0, 0, 0
+    #for X_test, y_test in self.test_dataloader:
     # _____NYUv2 start_____
     for item in test_dataloader:
-        eval_data, eval_label, eval_depth, eval_normal = item
-        X_outer = eval_data
-        y_outer = (eval_label, eval_depth, eval_normal)
-        # _____NYUv2 end_____
-        # Move to GPU
-        X_outer = X_outer.to(self.device)
-        if type(y_outer) is tuple:
-            nb_items = len(y_outer)
-            for i in range(nb_items):
-              l = list(y_outer)
-              l[i] = l[i].to(self.device)
-              y_outer = tuple(l)
-        else:
-            y_outer = y_outer.to(self.device)
-        # Inner value corresponds to h*(X_outer)
-        inner_value = self.inner_solution(mu, X_outer, y_outer)
-        if nb_iters<4:
-          loss = self.outer_loss(mu, inner_value, y_outer)
-        else:
-          loss = self.outer_loss(mu, inner_value, y_outer, accuracy=True)
-        running_loss += loss.item()
-        nb_iters += 1
-        #print("Outer test iteration:", nb_iters, "\nRunning loss:", running_loss/nb_iters)
-        losses.append(running_loss/nb_iters)
-    return nb_iters, losses
+      eval_data, eval_label, eval_depth, eval_normal = item
+      X_outer = eval_data
+      y_outer = (eval_label, eval_depth, eval_normal)
+      # _____NYUv2 end_____
+      # Move to GPU
+      X_outer = X_outer.to(self.device)
+      if type(y_outer) is tuple:
+        nb_items = len(y_outer)
+        for i in range(nb_items):
+          l = list(y_outer)
+          l[i] = l[i].to(self.device)
+          y_outer = tuple(l)
+      else:
+        y_outer = y_outer.to(self.device)
+      # Inner value corresponds to h*(X_outer)
+      inner_value = self.inner_solution(mu, X_outer, y_outer)
+      # Compute accuracy
+      seg, depth, normal = y_outer
+      seg_pred, depth_pred, pred_normal = inner_value
+      _, predicted = torch.max(seg_pred.data, 1)
+      value = seg.detach().flatten().cpu()
+      pred = predicted.detach().flatten().cpu()
+      correct = torch.sum(value==pred)
+      total = torch.sum(value!=-1)
+      acc = correct/total
+      total_acc += acc
+      # Compute loss
+      loss = self.outer_loss(mu, inner_value, y_outer)
+      total_loss += loss.item()
+      iters += 1
+      if iters >= max_iters:
+        break
+    return total_loss/iters, total_acc/iters

src/model/InnerSolution/InnerSolution.py
@@ -23,8 +23,10 @@ class InnerSolution(nn.Module):
     super(InnerSolution, self).__init__()
     self.inner_loss = inner_loss
     self.inner_dataloader = inner_dataloader
-    self.model, self.optimizer, self.dual_model, self.dual_optimizer = inner_models
+    self.model, self.optimizer, self.scheduler, self.dual_model, self.dual_optimizer, self.dual_scheduler = inner_models
     self.device = device
+    self.loss = 0
+    self.dual_loss = 0
   
   def forward(self, mu, X_outer, y_outer):
     """
@@ -38,11 +40,11 @@ class InnerSolution(nn.Module):
     opt_inner_val = ArgMinOp.apply(self, mu, X_outer, y_outer)
     return opt_inner_val
 
-  def optimize(self, mu, max_epochs=1):
+  def optimize(self, mu, max_epochs=1, max_iters=50):
     """
     Optimization loop for the inner-level model that approximates h*.
     """
-    running_loss, nb_iters, losses = 0, 0, []
+    epoch_loss, epoch_iters = 0, 0
     for epoch in range(max_epochs):
       #for X_inner, y_inner in self.inner_dataloader:
       # _____NYUv2 start_____
@@ -69,18 +71,18 @@ class InnerSolution(nn.Module):
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-        running_loss += loss.item()
-        nb_iters += 1
-        losses.append(running_loss/nb_iters)
-        if nb_iters >= 3:#test
+        epoch_loss += loss.item()
+        epoch_iters += 1
+        if epoch_iters >= max_iters:
           break
-    return nb_iters, losses
+      self.scheduler.step()
+      self.loss = epoch_loss/epoch_iters
   
-  def optimize_dual(self, mu, X_outer, y_outer, outer_grad2, max_epochs=1):
+  def optimize_dual(self, mu, X_outer, y_outer, outer_grad2, max_epochs=1, max_iters=50):
     """
     Optimization loop for the inner-level model that approximates a*.
     """
-    running_loss, nb_iters, losses = 0, 0, []
+    epoch_loss, epoch_iters = 0, 0
     for epoch in range(max_epochs):
       #for X_inner, y_inner in self.inner_dataloader:
       # _____NYUv2 start_____
@@ -109,12 +111,12 @@ class InnerSolution(nn.Module):
         self.dual_optimizer.zero_grad()
         loss.backward()
         self.dual_optimizer.step()
-        running_loss += loss.item()
-        nb_iters += 1
-        losses.append(running_loss/nb_iters)
-        if nb_iters >= 3:#test
+        epoch_loss += loss.item()
+        epoch_iters += 1
+        if epoch_iters >= max_iters:
           break
-    return nb_iters, losses
+      self.dual_scheduler.step()
+      self.dual_loss = epoch_loss/epoch_iters
 
   def loss_H(self, mu, h_X_i, a_X_i, a_X_o, y_inner, outer_grad2):
     """
@@ -132,15 +134,16 @@ class InnerSolution(nn.Module):
     if not(type(outer_grad2) is tuple):
       outer_grad2 = (outer_grad2,)
     # Specifying the inner objective as a function of h*(X)
-    f = lambda h_X: self.inner_loss(mu, h_X, y_inner)
+    #f = lambda h_X: self.inner_loss(mu, h_X, y_inner)
+    f = lambda item1, item2, item3: self.inner_loss(mu, (item1, item2, item3), y_inner)
     # Find the product with the hessian wrt h*(X)
     hessvp = autograd.functional.hvp(f, h_X_i, a_X_i)[1]
     # Compute the loss
     term1, term2 = 0, 0
     for i in range(nb_items):
-      term1 += torch.einsum('ij,ij->',a_X_i[i],hessvp[i])
+      term1 += torch.einsum('ijkl,ijkl->',a_X_i[i],hessvp[i])
     for i in range(nb_items):
-      term2 += torch.einsum('ij,ij->',a_X_o[i],outer_grad2[i])
+      term2 += torch.einsum('ijkl,ijkl->',a_X_o[i],outer_grad2[i])
     return (1/2)*term1+(1/2)*term2
 
   def compute_hessian_vector_prod(self, mu, X_outer, y_outer, inner_value, dual_val):
@@ -148,7 +151,8 @@ class InnerSolution(nn.Module):
     Computing B*a where a is dual_val=a(X_outer) and B is the functional derivative delta_mu delta_h g(mu,h*).
     """
     # Specifying the inner objective as a function of mu and h*(X)
-    f = lambda arg1, arg2: self.inner_loss(arg1, arg2, y_outer)
+    #f = lambda arg1, arg2: self.inner_loss(arg1, arg2, y_outer)
+    f = lambda mu, item1, item2, item3: self.inner_loss(mu, (item1, item2, item3), y_outer)
     # Make sure a*(X) is a tuple, if not, wrap as tuple.
     if not(type(dual_val) is tuple):
       dual_val = (dual_val,)
@@ -156,7 +160,7 @@ class InnerSolution(nn.Module):
     if not(type(inner_value) is tuple):
       inner_value = (inner_value,)
     # Here v has to be a tuple, so we concatinate mu with a*(X).
-    v = (mu.detach().clone()*0,) + dual_val
+    v = (torch.zeros_like(mu),) + dual_val
     # Here args has to be a tuple, so we concatinate mu with h*(X).
     args = (mu,) + inner_value
     # Similar to H_loss eigsum
@@ -190,10 +194,11 @@ class ArgMinOp(torch.autograd.Function):
     return inner_value
 
   @staticmethod
-  def backward(ctx, outer_grad2):
+  def backward(ctx, outer_grad2_0, outer_grad2_1, outer_grad2_2):
     """
     Backward pass of a function that approximates h* for Neur. Imp. Diff.
     """
+    outer_grad2 = (outer_grad2_0, outer_grad2_1, outer_grad2_2)
     # Context ctx allows to communicate from forward to backward
     inner_solution = ctx.inner_solution
     inner_value = ctx.inner_value

src/model/utils.py
@@ -77,22 +77,28 @@ def plot_1D_iterations(figname, iters1, iters2, f1, f2, plot_x_lim=[0,1], titles
   ax2.set_ylabel("f(\mu)")
   plt.savefig(figname+".png")
 
-def plot_loss(figname, loss_values, title="Step-wise Loss", labels=None):
+def plot_loss(figname, train_loss, val_loss, test_loss, title="Segmentation"):
   """
   Plot the loss value over iterations.
-    param loss_values: list of values to be plotted
+    param figname: name of the figure
+    param train_loss: list of train loss values
+    param aux_loss: list of auxiliary loss values
+    param test_loss: list of test loss values
   """
-  #loss_values = [tensor.item() for tensor in loss_values]
-  ticks = np.arange(0, len(loss_values), 1)
-  fig, ax = plt.subplots(figsize=(4,3))
-  plt.plot(ticks, loss_values)
+  # Generate a sequence of integers to represent the epoch numbers
+  epochs = len(train_loss)
+  ticks = np.arange(0, epochs, 1)
+  plt.xticks(ticks=ticks) 
+  # Plot and label the training and validation loss values
+  plt.plot(ticks, train_loss, label='Outer Loss')
+  plt.plot(ticks, val_loss, label='Inner Loss')
+  plt.plot(ticks, test_loss, label='Test Loss')  
+  # Add in a title and axes labels
   plt.title(title)
-  n_ticks = len(ticks)
-  if n_ticks > 10:
-    labels = ['']*n_ticks
-  plt.xticks(ticks=ticks, labels=labels)
-  plt.xlabel("Epochs")
-  plt.ylabel("Loss")
+  plt.xlabel('Epochs')
+  plt.ylabel('Loss')   
+  # Save the plot
+  plt.legend(loc='best')
   plt.savefig(figname+".png")
 
 def sample_X(X, n):
@@ -138,6 +144,8 @@ def tensor_to_state_dict(model, params, device):
   start = 0
   current_dict = model.state_dict()
   for name, param in model.named_parameters():
+    if name == 'layer_1.bias':
+      break
     dim = torch.tensor(param.size())
     length = torch.prod(dim, 0)
     dim = dim.tolist()

src/tests/nyu2_main.py
@@ -9,6 +9,7 @@ from torch import autograd
 from torch.autograd import grad
 from torch.func import functional_call
 from torch.nn import functional as func
+import torch.optim.lr_scheduler as lr_scheduler
 from torch.autograd.functional import hessian
 from torch.utils.data import Dataset, DataLoader
 from torchsummary import summary
@@ -21,10 +22,8 @@ from model.BilevelProblem.BilevelProblem import BilevelProblem
 from model.utils import set_seed, plot_loss, tensor_to_state_dict
 
 from experiments.NYuv2.experiments.nyuv2.data import nyu_dataloaders
-from experiments.NYuv2.experiments.nyuv2.metrics import compute_iou, compute_miou
 from experiments.NYuv2.experiments.nyuv2.model import SegNetSplit
-from experiments.NYuv2.auxilearn.hypernet import MonoHyperNet, MonoLinearHyperNet, MonoNonlinearHyperNet, MonoNoFCCNNHyperNet
-from experiments.NYuv2.auxilearn.optim import MetaOptimizer
+from model.NeuralNetworks.NeuralNetworkOuterModel import NeuralNetworkOuterModel
 
 # Setting the random seed.
 set_seed()
@@ -41,8 +40,8 @@ else:
     print("No GPUs found, setting the device to CPU.")
 
 # Setting hyper-parameters
-batch_size = 16
-max_epochs = 1
+batch_size = 8
+max_epochs = 2#200
 
 # Dataloaders for inner and outer data
 inner_dataloader, outer_dataloader, test_dataloader = nyu_dataloaders(
@@ -53,95 +52,74 @@ inner_dataloader, outer_dataloader, test_dataloader = nyu_dataloaders(
     val_batch_size=batch_size
 )
 
-#########################################################
-############ NEURAL IMPLICIT DIFFERENTIATION ############
-#########################################################
-
-# Helper function for objectives
-def calc_loss(seg_pred, seg, depth_pred, depth, pred_normal, normal):
-    """
-    Per-pixel loss, i.e., loss image
-    """
-    # binary mark to mask out undefined pixel space
-    binary_mask = (torch.sum(depth, dim=1) != 0).type(torch.FloatTensor).unsqueeze(1).to(depth.device)
-    # semantic loss: depth-wise cross entropy
-    seg_loss = func.nll_loss(seg_pred, seg, ignore_index=-1, reduction='none')
-    # depth loss: l1 norm
-    depth_loss = torch.sum(torch.abs(depth_pred - depth) * binary_mask, dim=1)
-    # normal loss: dot product
-    normal_loss = 1 - torch.sum((pred_normal * normal) * binary_mask, dim=1)
-    return (seg_loss, depth_loss, normal_loss)
-
 # Inner model
 # Neural network to approximate the function h*
 inner_model = SegNetSplit(logsigma=False).to(device)
 # Optimizer that improves the approximation of h*
-inner_optimizer = torch.optim.SGD(inner_model.parameters(), lr=0.001)
+inner_optimizer = torch.optim.Adam(inner_model.parameters(), lr=1e-4)
+inner_scheduler = lr_scheduler.StepLR(inner_optimizer, step_size=100, gamma=0.5)
 
 # Inner dual model
 # Neural network to approximate the function a*
 inner_dual_model = SegNetSplit(logsigma=False).to(device)
 # Optimizer that improves the approximation of a*
-inner_dual_optimizer = torch.optim.SGD(inner_model.parameters(), lr=0.001)
+inner_dual_optimizer = torch.optim.Adam(inner_model.parameters(), lr=1e-4)
+inner_dual_scheduler = lr_scheduler.StepLR(inner_optimizer, step_size=100, gamma=0.5)
 
 # Outer model
 # The outer neural network parametrized by the outer variable mu
-outer_model = MonoNoFCCNNHyperNet(main_task=0, reduction='mean').to(device)
+outer_model = (NeuralNetworkOuterModel(layer_sizes=[2,1])).to(device)
+# Initialize mu
+mu0 = torch.ones((2, 1)).to(device)#nn.Parameter(torch.ones((2, 1)), requires_grad=True).to(device)#torch.ones((2, 1)).to(device)
+# Optimizer that improves the outer variable mu
+outer_optimizer = torch.optim.SGD([mu0], lr=1e-1, momentum=.9, weight_decay=1e-5)
+outer_scheduler = lr_scheduler.StepLR(outer_optimizer, step_size=30, gamma=0.5)
 
-# Objective functions
-def fo(mu, h_X_out, y_out, accuracy=False):
+# Gather all models
+inner_models = (inner_model, inner_optimizer, inner_scheduler, inner_dual_model, inner_dual_optimizer, inner_dual_scheduler)
+outer_models = (outer_model, outer_optimizer, outer_scheduler)
+
+# Helper function for objectives
+def calc_loss(seg_pred, seg, depth_pred, depth, pred_normal, normal):
+    # binary mark to mask out undefined pixel space
+    binary_mask = (torch.sum(depth, dim=1) != 0).type(torch.FloatTensor).unsqueeze(1).to(depth.device)
+    n_nonzeros = (binary_mask != 0).sum(dim=(1, 2, 3))  # non-zeros per image
+    # semantic loss: depth-wise cross entropy
+    seg_loss = func.nll_loss(seg_pred, seg, ignore_index=-1, reduction='none').mean(dim=(1, 2))
+    # depth loss: l1 norm
+    depth_loss = torch.sum(torch.abs(depth_pred - depth) * binary_mask, dim=1).sum(dim=(1, 2)) / n_nonzeros
+    # normal loss: dot product
+    normal_loss = 1 - torch.sum((pred_normal * normal) * binary_mask, dim=1).sum(dim=(1, 2)) / n_nonzeros
+    return (seg_loss, depth_loss, normal_loss)
+
+# Outer objective function
+def fo(mu, h_X_out, y_out):
     seg, depth, normal = y_out
     seg_pred, depth_pred, pred_normal = h_X_out
     seg = seg.type(torch.LongTensor).to(device)
-    # Display accuracy
-    if accuracy:
-        _, predicted = torch.max(seg_pred.data, 1)
-        value = seg.detach().flatten().cpu()
-        pred = predicted.detach().flatten().cpu()
-        print("Accuracy:", torch.sum(value == pred)/value.size()[0])
     seg_loss, depth_loss, normal_loss = calc_loss(seg_pred, seg, depth_pred, depth, pred_normal, normal)
-    loss_image = torch.stack((seg_loss, depth_loss, normal_loss), dim=1)
-    total_loss = loss_image.mean(dim=(0, 2, 3))
-    loss = total_loss[0].mean(0)
+    loss_image = torch.column_stack((depth_loss.mean(0), normal_loss.mean(0)))
+    loss = seg_loss.mean(0) + 0*torch.sum(mu)
     return loss
 
+# Inner objective function
 def fi(mu, h_X_in, y_in):
     seg, depth, normal = y_in
     seg_pred, depth_pred, pred_normal = h_X_in
+    seg = seg.type(torch.LongTensor).to(device)
     seg_loss, depth_loss, normal_loss = calc_loss(seg_pred, seg, depth_pred, depth, pred_normal, normal)
-    loss_image = torch.stack((seg_loss, depth_loss, normal_loss), dim=1)
-    loss = functional_call(outer_model, tensor_to_state_dict(outer_model, mu, device), loss_image)
+    loss_image = torch.column_stack((depth_loss.mean(0), normal_loss.mean(0)))
+    loss = seg_loss.mean(0) + functional_call(outer_model, tensor_to_state_dict(outer_model, mu, device), loss_image)
     return loss
-
-# Initialize mu
-summary(outer_model, input_size=(3, 288, 384), device=device)
-mu0 = torch.randn((1408, 1))
-# Optimizer that improves the outer variable mu
-outer_optimizer = torch.optim.SGD([mu0], lr=0.1)
-
-# Gather all models
-inner_models = (inner_model, inner_optimizer, inner_dual_model, inner_dual_optimizer)
-outer_models = (outer_model, outer_optimizer)
 
 # Optimize using neural implicit differention
 bp_neural = BilevelProblem(fo, fi, outer_dataloader, inner_dataloader, outer_models, inner_models, device, batch_size=batch_size)
-nb_iters, iters, losses, times = bp_neural.optimize(mu0, max_epochs=max_epochs)
+iters, outer_losses, inner_losses, test_losses, times = bp_neural.optimize(mu0, max_epochs=max_epochs, test_dataloader=test_dataloader)
 
 # Show results
-print("Number of iterations:", nb_iters)
-#print("Outer variable values:", iters)
-print("Outer loss values:", losses)
+print("Number of iterations:", iters)
 print("Average iteration time:", np.average(times))
-print()
 
-plot_loss(figures_dir+"out_loss_NID", losses, title="Outer loss of neur. im. diff.")
+plot_loss(figures_dir+"out_loss_NID", outer_losses, inner_losses, test_losses, title="Losses of neur. im. diff.")
 
-test_losses = []
-for mu in iters:
-    nb_iters, losses = bp_neural.evaluate(test_dataloader, mu)
-    print("Losses for mu iteration:", losses)
-    test_losses.append(losses[-1])
-print("Test loss values:", test_losses)
-plot_loss(figures_dir+"test_loss_NID", test_losses, title="Test loss of neur. im. diff.")
-
-#Use gpu 23/28 rather than 21, more memory
+#Use gpu 23 or 28 rather than 21, more memory

src/tests/test_toy_aux_task.py
@@ -9,6 +9,7 @@ from torch import autograd
 from torch.autograd import grad
 from torch.func import functional_call
 from torch.autograd.functional import hessian
+import torch.optim.lr_scheduler as lr_scheduler
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 
@@ -102,10 +103,9 @@ inner_dataloader = DataLoader(dataset=inner_data, batch_size=batch, shuffle=True
 outer_data = Data(X_val, y_val)
 outer_dataloader = DataLoader(dataset=outer_data, batch_size=batch, shuffle=True)
 
-
-#########################################################
-############ NEURAL IMPLICIT DIFFERENTIATION ############
-#########################################################
+# Setting hyper-parameters
+batch_size = 8
+max_epochs = 50#200
 
 # Inner model
 layer_sizes = [2, 10, 20, 10, 1]
@@ -113,12 +113,14 @@ layer_sizes = [2, 10, 20, 10, 1]
 inner_model = (NeuralNetworkInnerModel(layer_sizes)).to(device)
 # Optimizer that improves the approximation of h*
 inner_optimizer = torch.optim.SGD(inner_model.parameters(), lr=0.01)
+inner_scheduler = lr_scheduler.StepLR(inner_optimizer, step_size=30, gamma=0.5)
 
 # Inner dual model
 # Neural network to approximate the function a*
 inner_dual_model = (NeuralNetworkInnerDualModel(layer_sizes)).to(device)
 # Optimizer that improves the approximation of a*
 inner_dual_optimizer = torch.optim.SGD(inner_dual_model.parameters(), lr=0.01)
+inner_dual_scheduler = lr_scheduler.StepLR(inner_dual_optimizer, step_size=30, gamma=0.5)
 
 # Outer model
 layer_sizes = [2, 1]
@@ -126,25 +128,22 @@ layer_sizes = [2, 1]
 outer_model = (NeuralNetworkOuterModel(layer_sizes)).to(device)
 # Optimizer that improves the outer variable mu
 outer_optimizer = torch.optim.SGD([mu0], lr=0.1)
+outer_scheduler = lr_scheduler.StepLR(outer_optimizer, step_size=30, gamma=0.5)
 
 # Gather all models
-inner_models = (inner_model, inner_optimizer, inner_dual_model, inner_dual_optimizer)
-outer_models = (outer_model, outer_optimizer)
+inner_models = (inner_model, inner_optimizer, inner_scheduler, inner_dual_model, inner_dual_optimizer, inner_dual_scheduler)
+outer_models = (outer_model, outer_optimizer, outer_scheduler)
 
 # Objective functions 
-fo = lambda mu, inner_value, y_out: ((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_out[:,0], (len(y_out),1))),2))) + 0*functional_call(outer_model, tensor_to_state_dict(outer_model, mu), torch.hstack((((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_out[:,1], (len(y_out),1))),2))),((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_out[:,2], (len(y_out),1))),2))))))
-fi = lambda mu, inner_value, y_in: ((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_in[:,0], (len(y_in),1))),2))) + functional_call(outer_model, tensor_to_state_dict(outer_model, mu), torch.hstack((((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_in[:,1], (len(y_in),1))),2))),((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_in[:,2], (len(y_in),1))),2))))))
+fo = lambda mu, inner_value, y_out: ((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_out[:,0], (len(y_out),1))),2))) + 0*functional_call(outer_model, tensor_to_state_dict(outer_model, mu, device), torch.hstack((((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_out[:,1], (len(y_out),1))),2))),((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_out[:,2], (len(y_out),1))),2))))))
+fi = lambda mu, inner_value, y_in: ((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_in[:,0], (len(y_in),1))),2))) + functional_call(outer_model, tensor_to_state_dict(outer_model, mu, device), torch.hstack((((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_in[:,1], (len(y_in),1))),2))),((1/2)*torch.mean(torch.pow((inner_value - torch.reshape(y_in[:,2], (len(y_in),1))),2))))))
 
 # Optimize using neural implicit differention
-bp_neural = BilevelProblem(fo, fi, outer_dataloader, inner_dataloader, outer_models, inner_models, device, batch_size=batch)
-nb_iters, iters, losses, times = bp_neural.optimize(mu0, max_epochs=max_epochs)
+bp_neural = BilevelProblem(fo, fi, outer_dataloader, inner_dataloader, outer_models, inner_models, device, batch_size=batch_size)
+iters, outer_losses, inner_losses, test_losses, times = bp_neural.optimize(mu0, max_epochs=max_epochs, test_dataloader=None)
 
 # Show results
-print("NEURAL IMPLICIT DIFFERENTIATION")
-print("Number of iterations:", nb_iters)
-print("Outer variable values:", iters)
-print("Outer loss values:", losses)
+print("Number of iterations:", iters)
 print("Average iteration time:", np.average(times))
-print()
 
-plot_loss(figures_dir+"out_loss_NID", losses, title="Outer loss of neur. im. diff.")
+plot_loss(figures_dir+"out_loss_NID", outer_losses, inner_losses, test_losses, title="Losses of neur. im. diff.")