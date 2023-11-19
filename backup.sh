c954f491f91e5740ded35eb0d0c54b92f8b6c01b
Ieva Petrulionyte <ipetruli@lp-hapi.inrialpes.fr>
2023-11-19 23:04:47 +0100

    daily updates

--- backup.sh
+++ backup.sh
@@ -2,8 +2,9 @@
 USER=ipetruli
 SERVER=edgar
 FOLDER=/home/clear/ipetruli/projects/bilevel-optimization/src
+# --exclude={'wandb', '*.stdout', '*.stderr'}
 
-rsync -Pavu --exclude={'wandb', '*.stdout', '*.stderr'} $USER@$SERVER:$FOLDER .
+rsync -Pavu $USER@$SERVER:$FOLDER .
 git add -u
 git commit -m "daily updates"
 git push
